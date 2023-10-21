85a5ba5f897ca5c140070283810774d473de2152
Eddie Halling <exel_eddie@hotmail.com>
2023-10-21 12:28:36 +0200

    Add project files.

---
+++ Labb 1 - Grunderna i OOP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Labb_1___Grunderna_i_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle.Run();
+        }
+        class Circle
+        {
+            public float _pi = 3.14159265359f;
+            public int _radius;
+
+            public Circle(int Radius)
+            {
+                _radius = Radius;
+            }
+            public void getArea()
+            {
+                float result = _radius * _radius * _pi;
+                Console.WriteLine($"Arean för cirkeln med en radius på 5cm är: {result}cm2");
+            }
+            public static void Run()
+            {
+                Circle circle1 = new Circle(5);
+                Circle circle2 = new Circle(6);
+                circle1.getArea();
+                circle2.getArea();
+            }
+        }
+    }
+}
